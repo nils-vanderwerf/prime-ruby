29ed18dd9428a343525e53871db604fedfacabde
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-02 15:44:17 +1000

    Created prime number function

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime? num
+    n = 2
+    return false if num <= 1
+    while n < num 
+        return false if num % n == 0
+        n += 1
+    end
+        true
+    end
+
+puts prime?(19)
+puts prime?(14)
+puts prime?(1000)
+puts prime?(-1000)
